1ee9f241c5eb9200aa19447f9ebd7f9768cd06e8
Vanio Miguel Ferreira <vanio-miguel.ferreira@ext.consilium.europa.eu>
2025-03-01 14:55:30 +0100

    added backend

---
+++ be/src/main/java/com/pcompass/gfspa/GfspaApplication.java
@@ -0,0 +1,13 @@
+package com.pcompass.gfspa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GfspaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GfspaApplication.class, args);
+	}
+
+}
---
+++ be/src/test/java/com/pcompass/gfspa/GfspaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcompass.gfspa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GfspaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
